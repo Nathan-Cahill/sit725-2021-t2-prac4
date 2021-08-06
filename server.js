f6207af85189bde9a5769fd9b052b5bd66580e84
Nathan Cahill <cahilln@deakin.edu.au>
2021-08-06 12:55:16 +1000

    added basic code in server.js and added env.js

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 var express = require("express");
 var app = express()
 
-app.use(express.static(__dirname+'/public'))
 
-var port = process.env.port || 3000;
+let http = require('http').createServer(app);
+let io = require('socket.io')(http);
+const bodyParser = require('body-parser');
+
+
+
+
+var port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname+'/public'));
+app.use(bodyParser.json());
+
+
+app.get('/api/projects',(req,res)=>{
+    console.log('project requested')
+    res.send(dummyData)
+})
+
+
 
 app.listen(port,()=>{
     console.log("Application has started on port: ", port)
